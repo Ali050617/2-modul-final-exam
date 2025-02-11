1115c748a8d46d33a41405ce64fd4e617c16178b
Muhammadali <muhammadalikadyrov06@gmail.com>
2025-02-11 23:01:20 +0500

    Validation failed for the number

--- departments/forms.py
+++ departments/forms.py
@@ -29,9 +29,10 @@ class DepartmentForm(forms.ModelForm):
             }),
             'phone_number': forms.TextInput(attrs={
                 'class': 'w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500',
-                'placeholder': 'Enter contact phone',
+                'placeholder': 'The phone number must be in the format +998XXXXXXXXX.',
             }),
         }
+
 
     def clean_name(self):
         name = self.cleaned_data.get('name')
